3fd09d9126b4fd5e5efd460f193da0022089803f
duilmarc <franco.chp@gmail.com>
2020-11-18 22:37:54 -0500

    deploy:heroku

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Frontend
+# AngularOnHeroku
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.7.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.4.
 
 ## Development server
 
@@ -25,4 +25,3 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
-# shell-interpreter-python
--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-
 const express = require('express');
 const path = require('path');
- 
 const app = express();
- 
-// Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/dist/frontend'));
- 
+app.use(express.static(__dirname + '/dist/angular-on-heroku'));
 app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname+'/src/index.html'));
-});
- 
-// Start the app by listening on the default Heroku port
+  res.sendFile(path.join(__dirname+
+    '/dist/angular-on-heroku/index.html'));});
 app.listen(process.env.PORT || 8080);
